8a749a95e0f9702c7ef1b1c29693ab22b49a496e
Elicas5931 <elizabeth.perry.339@gmail.com>
2019-03-06 20:09:58 -0500

    Fixing up application, adoptables, and bio pages

--- client/src/pages/Application/Application.js
+++ client/src/pages/Application/Application.js
@@ -13,21 +13,21 @@ class ApplicationPage extends Component {
                 <br></br>
                 <form>
                     <div className='form-group row'>
-                        <label for='inputName' className='col-sm-2 col-form-label'>Name</label>
+                        <label htmlFor='inputName' className='col-sm-2 col-form-label'>Name</label>
                         <div className='col-sm-8'>
                             <input type='text' className='form-control' id='inputName' placeholder='Name'></input>
                         </div>
                     </div>
 
                     <div className='form-group row'>
-                        <label for='inputAddress' className='col-sm-2 col-form-label'>Address</label>
+                        <label htmlFor='inputAddress' className='col-sm-2 col-form-label'>Address</label>
                         <div className='col-sm-8'>
                             <input type='text' className='form-control' id='inputAddress' placeholder='1234 Main St'></input>
                         </div>
                     </div>
 
                     <div className='form-group row'>
-                        <label for='inputAddress2' className='col-sm-2 col-form-label'>Address 2</label>
+                        <label htmlFor='inputAddress2' className='col-sm-2 col-form-label'>Address 2</label>
                         <div className='col-sm-8'>
                             <input type='text' className='form-control' id='inputAddress2' placeholder='Apartment, studio, floor'></input>
                         </div>
@@ -35,32 +35,32 @@ class ApplicationPage extends Component {
 
                     <div className='form-group row' id='cityState'>
                         <div className="form-group col-md-3">
-                            <label for="inputCity">City</label>
+                            <label htmlFor="inputCity">City</label>
                             <input type="text" className="form-control" id="inputCity"></input>
                         </div>
                         <div className="form-group col-md-4">
-                            <label for="inputState">State</label>
-                            <select id="inputState" className="form-control">
+                            <label htmlFor="inputState">State</label>
+                            <select id="inputState" className="form-control" defaultValue='state'>
                                 <option selected>Choose...</option>
                                 <option>North Carolina</option>
                                 <option>South Carolina</option>
                             </select>
                         </div>
                         <div className="form-group col-md-2">
-                            <label for="inputZip">Zip</label>
+                            <label htmlFor="inputZip">Zip</label>
                             <input type="text" className="form-control" id="inputZip"></input>
                         </div>
                     </div>
 
                     <div className='form-group row'>
-                        <label for='inputPhone' className='col-sm-2 col-form-label'>Phone Number</label>
+                        <label htmlFor='inputPhone' className='col-sm-2 col-form-label'>Phone Number</label>
                         <div className='col-sm-8'>
                             <input type='text' className='form-control' id='inputPhone' placeholder='(* * *) * * * - * * * *'></input>
                         </div>
                     </div>
 
                     <div className='form-group row'>
-                        <label for='inputEmail' className='col-sm-2 col-form-label'>Email</label>
+                        <label htmlFor='inputEmail' className='col-sm-2 col-form-label'>Email</label>
                         <div className='col-sm-8'>
                             <input type='email' className='form-control' id='inputEmail' placeholder='JohnDoe@example.com'></input>
                         </div>
@@ -69,7 +69,7 @@ class ApplicationPage extends Component {
                     <h2>About You</h2>
 
                     <div className='form-group row'>
-                        <label for='inputOccupation' className='col-sm-2 col-form-label'>Occupation</label>
+                        <label htmlFor='inputOccupation' className='col-sm-2 col-form-label'>Occupation</label>
                         <div className='col-sm-4'>
                             <input type='text' className='form-control' id='inputOccupation' placeholder='Job Type'></input>
                         </div>
@@ -81,22 +81,22 @@ class ApplicationPage extends Component {
                     <br></br>
                     <fieldset className='form-group'>
                         <div className='row'>
-                            <legend class='col-form-label col-sm-2 pt-0' id='livingStatus'>Housing: </legend>
+                            <legend className='col-form-label col-sm-2 pt-0' id='livingStatus'>Housing: </legend>
                             <div className='form-check form-check-inline'>
                                 <input className='form-check-input' type='radio' name='inlineRadioOptions' id='inlineHouse' value='option1'></input>
-                                <label className='form-check-label' for='inlineHouse'>House</label>
+                                <label className='form-check-label' htmlFor='inlineHouse'>House</label>
                             </div>
                             <div className='form-check form-check-inline'>
                                 <input className='form-check-input' type='radio' name='inlineRadioOptions' id='inlineApartment' value='option2'></input>
-                                <label className='form-check-label' for='inlineApartment'>Apartment</label>
+                                <label className='form-check-label' htmlFor='inlineApartment'>Apartment</label>
                             </div>
                             <div className='form-check form-check-inline'>
                                 <input className='form-check-input' type='radio' name='inlineRadioOptions' id='inlineTownhome' value='option3'></input>
-                                <label className='form-check-label' for='inlineTownhome'>Townhome</label>
+                                <label className='form-check-label' htmlFor='inlineTownhome'>Townhome</label>
                             </div>
                             <div className='form-check form-check-inline'>
                                 <input className='form-check-input' type='radio' name='inlineRadioOptions' id='inlineOther' value='option4'></input>
-                                <label className='form-check-label' for='inlineOther'>Other</label>
+                                <label className='form-check-label' htmlFor='inlineOther'>Other</label>
                             </div>
                         </div>
                     </fieldset>
@@ -106,11 +106,11 @@ class ApplicationPage extends Component {
                             <legend class='col-form-label col-sm-2 pt-0' id='rentOwn'>Do you : </legend>
                             <div className='form-check form-check-inline'>
                                 <input className='form-check-input' type='radio' name='inlineRadioOptions2' id='inlineOwn' value='option1'></input>
-                                <label className='form-check-label' for='inlineOwn'>Own</label>
+                                <label className='form-check-label' htmlFor='inlineOwn'>Own</label>
                             </div>
                             <div className='form-check form-check-inline'>
                                 <input className='form-check-input' type='radio' name='inlineRadioOptions2' id='inlineRent' value='option2'></input>
-                                <label className='form-check-label' for='inlineRent'>Rent</label>
+                                <label className='form-check-label' htmlFor='inlineRent'>Rent</label>
                             </div>
                         </div>
                     </fieldset>
@@ -121,19 +121,19 @@ class ApplicationPage extends Component {
                             <legend class='col-form-label col-sm-2 pt-0' id='question1'>How many people live with you?</legend>
                             <div className='form-check form-check-inline'>
                             <input className='form-check-input' type='radio' name='inlineRadioQuestions' id='questionValue1' value='question1'></input>
-                            <label className='form-check-label' for='questionValue1'>0</label>
+                            <label className='form-check-label' htmlFor='questionValue1'>0</label>
                             </div>
                             <div className='form-check form-check-inline'>
                             <input className='form-check-input' type='radio' name='inlineRadioQuestions' id='questionValue2' value='question1'></input>
-                            <label className='form-check-label' for='questionValue2'>1</label>
+                            <label className='form-check-label' htmlFor='questionValue2'>1</label>
                             </div>
                             <div className='form-check form-check-inline'>
                             <input className='form-check-input' type='radio' name='inlineRadioQuestions' id='questionValue3' value='question1'></input>
-                            <label className='form-check-label' for='questionValue3'>2</label>
+                            <label className='form-check-label' htmlFor='questionValue3'>2</label>
                             </div>
                             <div className='form-check form-check-inline'>
                             <input className='form-check-input' type='radio' name='inlineRadioQuestions' id='questionValue4' value='question1'></input>
-                            <label className='form-check-label' for='questionValue4'>3 or More</label>
+                            <label className='form-check-label' htmlFor='questionValue4'>3 or More</label>
                             </div>
                         </div>
                     </div>
